de4959f57e3add4c2b8fd00b16481a999c8e51eb
ssweezy <aslanokaraf@gmail.com>
2025-11-11 05:38:13 +0300

    -fixed some mistakes test4.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import TourPage from "./pages/TourPage/TourPage.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
-import { useTelegramWindow } from './hooks/useTelegramWindow';
+import useTelegramWindow from './hooks/useTelegramWindow';
 import { Routes, Route} from "react-router";
 import { useEffect } from 'react';
 import WebApp from '@twa-dev/sdk'
