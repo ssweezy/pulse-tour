dd6fa74712d0d007d4276fc43c21bd8274f27772
ssweezy <aslanokaraf@gmail.com>
2025-11-11 06:05:29 +0300

    -lil fixes TEST5.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import TourPage from "./pages/TourPage/TourPage.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
-import useTelegramWindow from './hooks/useTelegramWindow';
-import { Routes, Route} from "react-router";
-import { useEffect } from 'react';
-import WebApp from '@twa-dev/sdk'
-
+import useTelegramWindow from "./hooks/useTelegramWindow";
+import { Routes, Route } from "react-router";
+import { useEffect } from "react";
+import WebApp from "@twa-dev/sdk";
 
 function App() {
-
-  WebApp.showAlert("AYO IM ALIVE")
-
-  useTelegramWindow()
-
+  useTelegramWindow();
 
   return (
     <>
