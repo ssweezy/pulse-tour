2cfd03925fa06e5934a6705038cfaff893267f7f
ssweezy <aslanokaraf@gmail.com>
2025-12-01 06:31:01 +0300

    - started work on tg initInfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import TourPage from "./pages/TourPage/TourPage.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import PayPage from "./pages/PayPage/PayPage.jsx";
-import useTelegramWindow from "./hooks/useTelegramWindow";
+import {useTelegramWindow} from "./hooks/useTelegramWindow";
 import { Routes, Route } from "react-router";
+import WebApp from "@twa-dev/sdk"
 
 function App() {
-  useTelegramWindow();
+  let tg = WebApp
+  let user = tg.initDataUnsafe?.user
+
+  useTelegramWindow()
+
+  // тестовый вызов входящих данных пользователя
+  let username = user.username
+  let id = user.id
+  let initInfo = tg.initDataUnsafe
+  tg.showAlert(`Привет, ${username} \nID - ${id} \n\n ${initInfo}`)
+  console.log(initInfo)
+
+
 
   return (
     <>
